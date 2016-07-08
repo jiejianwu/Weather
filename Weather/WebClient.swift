9e6226e25d1e114b54de953d16d671140550daa0
吴杰健 <shadow@kangkanghui.com>
2016-07-08 16:05:42 +0800

    import pod, import afnetworking, add subclass webclient

---
+++ Weather/WebClient.swift
@@ -0,0 +1,32 @@
+//
+//  WebClient.swift
+//  Weather
+//
+//  Created by 吴杰健 on 16/7/8.
+//  Copyright © 2016年 吴杰健. All rights reserved.
+//
+
+import Foundation
+
+
+class WebClient: AFHTTPSessionManager {
+
+    static let baseUrl = ""
+    static var _shareInstance: WebClient?
+    
+    class func shareInstance() -> WebClient {
+        if _shareInstance == nil {
+            _shareInstance = WebClient(baseURL: NSURL(string: baseUrl))
+        }
+        return _shareInstance!
+    }
+    
+    class func get(url: String, parameters: AnyObject?, success: ((NSURLSessionDataTask, AnyObject?) -> Void)?, failure: ((NSURLSessionDataTask?, NSError) -> Void)?) {
+        shareInstance().GET(url, parameters: parameters, progress: nil, success: success, failure: failure)
+    }
+    
+    class func post(url: String, parameters: AnyObject?, success: ((NSURLSessionDataTask, AnyObject?) -> Void)?, failure: ((NSURLSessionDataTask?, NSError) -> Void)?) {
+        shareInstance().POST(url, parameters: parameters, progress: nil, success: success, failure: failure)
+    }
+    
+}
